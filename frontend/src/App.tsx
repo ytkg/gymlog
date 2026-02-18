b46ffbcb3395167646215b5521d531897ce13854
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2026-02-18 14:00:25 +0900

    Add frontend React Vite TypeScript project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>React + Vite + TypeScript</h1>
+      <p>frontend project is ready.</p>
+    </main>
+  )
+}
+
+export default App
